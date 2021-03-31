82fa939680affee7c4861cf00530387b71f0168c
Federico Creti <federico.creti@solaredge.com>
2021-03-31 11:03:21 +0200

    Used a method instead of a combine flow to retrieve users

--- app/src/main/java/it/thefedex87/networkboundresourcestest/ui/main/MainViewModel.kt
+++ app/src/main/java/it/thefedex87/networkboundresourcestest/ui/main/MainViewModel.kt
@@ -1,11 +1,15 @@
 package it.thefedex87.networkboundresourcestest.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import it.thefedex87.networkboundresourcestest.data.Repository
+import it.thefedex87.networkboundresourcestest.data.db.User
 import it.thefedex87.networkboundresourcestest.util.Refresh
+import it.thefedex87.networkboundresourcestest.util.Resource
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
@@ -21,34 +25,54 @@ class MainViewModel @Inject constructor(
     val query: MutableStateFlow<String> = MutableStateFlow("")
     private val refresh: MutableSharedFlow<Refresh> = MutableSharedFlow()
 
-    val users = combine(
-        query,
-        refresh
-    ) { query, refresh ->
-        Pair(query, refresh)
-    }.flatMapLatest { (query, refresh) ->
-        repository.users(
-            query = query,
-            forceReload = refresh == Refresh.FORCE,
-            onFetchError = {
-                viewModelScope.launch {
-                    _mainShared.emit(MainStateEvent.Error(it))
-                }
+    init {
+        viewModelScope.launch {
+            query.collect {
+                getUsers(
+                    it,
+                    Refresh.DEFAULT
+                )
             }
-        )
-    }.stateIn(viewModelScope, SharingStarted.Lazily, null).asLiveData()
+        }
+    }
+
+    val users: MutableLiveData<Resource<List<User>>> = MutableLiveData()
 
     fun onStart() {
-        viewModelScope.launch {
-            delay(1)
-            refresh.emit(Refresh.DEFAULT)
+        getUsers(
+            query.value,
+            Refresh.DEFAULT
+        )
+    }
+
+    private lateinit var job: Job
+    private fun getUsers(
+        query: String,
+        refresh: Refresh
+    ) {
+        if(this::job.isInitialized) {
+            job.cancel()
+        }
+        job = viewModelScope.launch {
+            repository.users(
+                query = query,
+                forceReload = refresh == Refresh.FORCE,
+                onFetchError = {
+                    viewModelScope.launch {
+                        _mainShared.emit(MainStateEvent.Error(it))
+                    }
+                }
+            ).collect {
+                users.value = it
+            }
         }
     }
 
     fun updateUsers() {
-        viewModelScope.launch {
-            refresh.emit(Refresh.FORCE)
-        }
+        getUsers(
+            query.value,
+            Refresh.FORCE
+        )
     }
 }
 
